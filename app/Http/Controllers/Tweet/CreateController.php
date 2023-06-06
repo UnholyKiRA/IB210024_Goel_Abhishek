a22014d57adc63159b76b434d32b83b214616289
UnholyKiRA <abhichigo121@gmail.com>
2023-06-06 13:51:18 +0900

    2023/06/06

---
+++ app/Http/Controllers/Tweet/CreateController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Tweet;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests\Tweet\CreateRequest;
+use App\Models\Tweet;
+
+class CreateController extends Controller
+{
+ public function b(CreateRequest $request){
+    $tweets = new Tweet;
+    $tweets->content =$request->tweet();
+    $tweets->save();
+    return redirect()->route('tweet.index');
+ }
+}
